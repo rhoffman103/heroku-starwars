d25a89678872dc5bdb69b30fda50d2cd75305ffb
Bobby H <rhoffman103@gmail.com>
2018-09-08 13:45:24 -0400

    dynamic port

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
